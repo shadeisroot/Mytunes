5eacfa6d81314cf64cd4eb0579a13480e0c99a6a
shadeisroot <dschristensen@proton.me>
2023-12-04 11:31:44 +0100

    swap position to new button

--- src/main/java/com/example/mytunes/SongDaoimpl.java
+++ src/main/java/com/example/mytunes/SongDaoimpl.java
@@ -6,11 +6,11 @@ import javafx.scene.control.TextArea;
 
 import java.sql.*;
 import javafx.scene.control.TextArea;
-public class SongDaoimpl implements SongDao{
+public class SongDaoimpl implements SongDao {
     private Connection con;
 
     public SongDaoimpl() {
-        try{
+        try {
             con = DriverManager.getConnection("jdbc:sqlserver://10.176.111.34:1433;database=Mytunesgrp2;userName=CSe2023t_t_2;password=CSe2023tT2#23;encrypt=true;trustServerCertificate=true");
 
         } catch (SQLException e) {
@@ -20,11 +20,11 @@ public class SongDaoimpl implements SongDao{
 
     @Override
     public boolean saveSong(Song song) {
-        try{
+        try {
             PreparedStatement ps = con.prepareStatement("INSERT INTO Songs VALUES(?,?,?,?);");
             ps.setString(1, song.getTitle());
-            ps.setString(2,song.getArtist());
-            ps.setString(3,song.getGenre());
+            ps.setString(2, song.getArtist());
+            ps.setString(3, song.getGenre());
             ps.setDouble(4, song.getLength());
             ps.executeUpdate();
             System.out.println(("Song Created"));
@@ -40,37 +40,42 @@ public class SongDaoimpl implements SongDao{
     public void getAllPlaylists(ObservableList<Playlist> tabeldata) {
         tabeldata.clear();
         int antal = 0;
-        try{
+        try {
             Statement database = con.createStatement();
             String sql = "SELECT * FROM Playlist";
             ResultSet rs = database.executeQuery(sql);
             while (rs.next()) {
-                String name     = rs.getString("name");
-                Integer songs    = Integer.valueOf(rs.getString("songs"));
-                Double length     = Double.valueOf(rs.getString("length"));
+                String name = rs.getString("name");
+                Integer songs = Integer.valueOf(rs.getString("songs"));
+                Double length = Double.valueOf(rs.getString("length"));
 
                 Playlist playlist = new Playlist(name, songs, length);
                 tabeldata.add(playlist);
                 ++antal;
             }
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.err.println("Kan ikke læse records");
         }
 
     }
 
     public boolean deletePlaylist(Playlist playlist) {
-        try{
+        try {
             Statement database = con.createStatement();
             String sql = "DELETE FROM Playlist WHERE name = '" + playlist.getName() + "'";
             database.executeUpdate(sql);
             return true;
-        } catch (SQLException e){
-            System.err.println("Sletning lykkedes ikke"+e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Sletning lykkedes ikke" + e.getMessage());
             return false;
 
-    public void deleteSong(Song song){
-        try{
+
+        }
+
+    }
+
+    public void deleteSong(Song song) {
+        try {
             PreparedStatement ps = con.prepareStatement("DELETE FROM Songs WHERE Titel = ?");
             ps.setString(1, song.getTitle());
             ps.executeUpdate();
