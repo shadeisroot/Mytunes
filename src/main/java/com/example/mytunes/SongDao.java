741ca1e0c000e06233833553111b56c31d2547e0
Malene1603 <143804625+Malene1603@users.noreply.github.com>
2023-12-04 10:23:28 +0100

    Playlist tableview og playlist delete button

--- src/main/java/com/example/mytunes/SongDao.java
+++ src/main/java/com/example/mytunes/SongDao.java
@@ -1,5 +1,11 @@
 package com.example.mytunes;
 
+import javafx.collections.ObservableList;
+
 public interface SongDao {
     public boolean saveSong(Song song);
+
+    void getAllPlaylists(ObservableList<Playlist> tabeldata);
+
+    boolean deletePlaylist(Playlist p);
 }
